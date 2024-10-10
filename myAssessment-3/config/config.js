483aa8b1148a85062e429d5ee97b3a842072e57c
Luchenchen1 <145833354+Luchenchen1@users.noreply.github.com>
2024-10-10 11:50:16 +0800

    create

---
+++ myAssessment-3/config/config.js
@@ -0,0 +1,11 @@
+// This is the configuration file for the database
+module.exports = {
+	// Host Name Host name of the computer where the database is located
+	host:'localhost',
+	user:'root',
+	password:'123456',
+	database:'crowdfunding_db',
+	port:'3306',
+	connectionLimit : 20,
+	waitForConnections: false
+}
