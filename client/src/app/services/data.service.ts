26e6099ade06d61c17dc33f153df5de8960997c2
Luchenchen1 <145833354+Luchenchen1@users.noreply.github.com>
2024-10-12 14:17:12 +0800

    Update data.service.ts
    search

--- client/src/app/services/data.service.ts
+++ client/src/app/services/data.service.ts
@@ -16,6 +16,21 @@ export class DataService {
     return this.http.get('http://localhost:3000/fundraisers');
   }
 
+  searchFundraisers(criteria: any): Observable<any[]> {
+    const params = this.serializeCriteria(criteria);
+    // 反引号构建url
+    return this.http.get<any[]>(`http://localhost:3000/search?${params}`);
+  }
 
+  //构建查询url
+  private serializeCriteria(criteria: any): string {
+    const params = new URLSearchParams();
+    if (criteria.organizer) params.append('organizer', criteria.organizer);
+    if (criteria.city) params.append('city', criteria.city);
+    criteria.categories.forEach((category: any) => {
+      if (category.selected) params.append('categories', category.name);
+    });
+    return params.toString();
+  }
 
 }
