5efe24c175e873e111bfc1cb412729601a66a5f4
Luchenchen1 <145833354+Luchenchen1@users.noreply.github.com>
2024-10-12 11:21:12 +0800

    update home

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-interface Fundraiser {
-  CAPTION: string;
-  ORGANIZER: string;
-  TARGET_FUNDING: number;
-  CURRENT_FUNDING: number;
-  CITY: string;
-  category_name: string;
-  imageUrl?: string;
-}
 
 
 @Component({
@@ -18,26 +9,6 @@ interface Fundraiser {
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  welcomeMessage: string = "Welcome to Our Funny Funding!";
-  inspiringStories: string = "Your support fuels our journey forward.";
-  contactInfo: string = "Contact us: info@nonprofit.org";
-  activeFundraisers: Fundraiser[] = [];
-
-  constructor(private http: HttpClient) {
-
-  }
-
-  ngOnInit(): void {
-    // 你可以在 ngOnInit 方法或其他任何方法中使用 this.http 来发送请求
-    this.http.get<Fundraiser[]>('http://localhost:3000/fundraisers').subscribe({
-        // 处理返回的数据
-        next: (data: Fundraiser[]) => this.activeFundraisers = data,
-        error: (error) => console.error('Error fetching active fundraisers:', error)
-  }
-
-      
-    );
-  }
-
+  
 
 }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Fundraiser {
+  CAPTION: string;
+  ORGANIZER: string;
+  TARGET_FUNDING: number;
+  CURRENT_FUNDING: number;
+  CITY: string;
+  category_name: string;
+  imageUrl?: string;
+}
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +19,23 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  welcomeMessage: string = "Welcome to Our Funny Funding!";
+  inspiringStories: string = "Your support fuels our journey forward.";
+  contactInfo: string = "Contact us: info@nonprofit.org";
+  activeFundraisers: Fundraiser[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    // 你可以在 ngOnInit 方法或其他任何方法中使用 this.http 来发送请求
+    this.http.get<Fundraiser[]>('http://localhost:3000/fundraisers').subscribe({
+        // 处理返回的数据
+        next: (data: Fundraiser[]) => this.activeFundraisers = data,
+        error: (error) => console.error('Error fetching active fundraisers:', error)
+  }
+      
+    );
+  }
 
 }
