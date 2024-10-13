5151644c56d41841901d09dfc8831ebf91adc276
Luchenchen1 <145833354+Luchenchen1@users.noreply.github.com>
2024-10-13 19:37:53 +0800

    add admin

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
+  
+  fundraisers:any;
+
+  // 构造函数中注入DataService
+  constructor(private dataService: DataService) {}
+
+  // ngOnInit生命周期钩子中调用DataService的方法
+  ngOnInit() {
+    this.dataService.getAllFundraisers().subscribe(
+      (fundraisers) => {
+        // 当数据成功返回时，将其赋值给data属性
+        this.fundraisers=  fundraisers;
+      },
+      (error) => {
+        // 处理错误情况
+        console.error('Error fetching data: ', error);
+      }
+    );
+  }
+
+  deleteFundraiser(fundraiser:any){
+
+
+  }
+
+  updateFundraiser(fundraiser:any){
+
+  }
 
 }
